5a0d0f25a46628f3c59403024cd8039fcb3ad8b2
auho <auho@163.com>
2020-05-21 10:44:15 +0800

    dev

--- _bootstrap.php
+++ _bootstrap.php
@@ -5,3 +5,5 @@ define('TESTS_BASE_PATH', __DIR__);
 define('CODECEPT_BASE_PATH', TESTS_BASE_PATH . DIRECTORY_SEPARATOR . 'codeception');
 
 \Codeception\Util\Autoload::addNamespace('codecept', CODECEPT_BASE_PATH . DIRECTORY_SEPARATOR . 'codecept');
+\Codeception\Util\Autoload::addNamespace('api', TESTS_BASE_PATH . DIRECTORY_SEPARATOR . 'api');
+\Codeception\Util\Autoload::addNamespace('common', TESTS_BASE_PATH . DIRECTORY_SEPARATOR . 'common');
